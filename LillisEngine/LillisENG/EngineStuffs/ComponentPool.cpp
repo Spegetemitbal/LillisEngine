15857bc14df27239b7c23a95e72a44dc705598ab
Aesithr <LeSkree@gmail.com>
2024-05-22 12:24:08 -0400

    Started on component pool

--- LillisEngine/LillisENG/EngineStuffs/ComponentPool.cpp
+++ LillisEngine/LillisENG/EngineStuffs/ComponentPool.cpp
@@ -1,42 +1,81 @@
 #include "pch.h"
 #include "ComponentPool.h"
 
-ComponentPool::ComponentPool()
+template <typename Obj>
+Obj* ComponentPool<Obj>::AllocateObj(char* base)
 {
-	mPool = new Component[100];
+	size_t sizeToAllocate = sizeof(Obj);
+	return new (base)Obj();
+}
+
+template <typename Obj>
+ComponentPool<Obj>::ComponentPool()
+{
 	count = 100;
+	poolDir.reserve(count);
+	mPool = new char[sizeof(Obj) * 100];
+
+	size_t sizeToAllocate = sizeof(Obj);
+	char* base = mPool;
+	for (int i = 0; i < count; i++)
+	{
+		try
+		{
+			poolDir.push_back(AllocateObj(base));
+		}
+		catch (...)
+		{
+			std::cout << "Non Component Type!, Failure imminent.";
+			exit(1);
+		}
+		base += sizeToAllocate;
+	}
 }
 
-ComponentPool::ComponentPool(unsigned int num)
+template <typename Obj>
+ComponentPool<Obj>::ComponentPool(unsigned int num)
 {
-	mPool = new Component[num];
 	count = num;
-}
+	poolDir.reserve(count);
+	mPool = new char[sizeof(Obj) * num];
 
-Component ComponentPool::AddComponent()
-{
-	if (activeLine < count)
+	size_t sizeToAllocate = sizeof(Obj);
+	char* base = mPool;
+	for (int i = 0; i < count; i++)
 	{
-		mPool[activeLine] = Component();
-	}
-	else
-	{
-		ResizePool();
-
+		try
+		{
+			poolDir.push_back(AllocateObj(base));
+		}
+		catch (...)
+		{
+			std::cout << "Non Component Type!, Failure imminent.";
+			exit(1);
+		}
+		base += sizeToAllocate;
 	}
 }
 
-void ComponentPool::DestroyComponent(Component& comp)
+template <typename Obj>
+Component ComponentPool<Obj>::AddComponent()
 {
 
 }
 
-void ComponentPool::SortPool()
+template <typename Obj>
+void ComponentPool<Obj>::DestroyComponent(Component& comp)
 {
 
 }
 
-void ComponentPool::ResizePool()
+template <typename Obj>
+void ComponentPool<Obj>::SortPool()
+{
+
+}
+
+template <typename Obj>
+void ComponentPool<Obj>::ResizePool()
 {
 
 }
--- LillisEngine/LillisENG/EngineStuffs/ComponentPool.h
+++ LillisEngine/LillisENG/EngineStuffs/ComponentPool.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Component.h"
 
+template <typename Obj>
 class ComponentPool
 {
 public:
@@ -11,13 +12,20 @@ public:
 	Component AddComponent();
 	void DestroyComponent(Component& comp);
 
+	//Typecast to specific component in game loop.
+	std::vector<Component*> poolDir;
+
 private:
 
 	void SortPool();
 	void ResizePool();
 
+	Obj* AllocateObj(char* base);
+
+	bool isGameObj = false;
+
 	unsigned int activeLine = 0;
 	unsigned int count = 0;
-	Component* mPool;
+	char* mPool = nullptr;
 };
 
